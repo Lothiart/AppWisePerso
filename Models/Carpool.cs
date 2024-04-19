7b8a114a34eb661c7394e16956a6efd2e783b1e5
Lothiart <cailllet.alex@gmail.com>
2024-04-19 10:55:24 +0200

    test

---
+++ Models/Carpool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Carpool
+    {
+        public int Id { get; set; }
+        public int NbPlacesRemain { get; set; }
+        public DateTime DepartureDate { get; set; }
+    }
+}
---
+++ Models/Vehicule.cs
@@ -0,0 +1,26 @@
+﻿namespace Models
+{
+    public class Vehicule
+    {
+        public int Id { get; set; }
+        public string Registration {  get; set; }
+        public int NbPlaces {  get; set; }
+        public int CO2 { get; set; }
+
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public int ModelId { get; set; }
+        public Model Model { get; set; }
+
+        public int MotorId { get; set; }
+        public Motor Motor { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
