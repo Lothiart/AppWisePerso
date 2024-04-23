74ca57a90d4923630322e4c97bd4a367f3817dd6
Lothiart <cailllet.alex@gmail.com>
2024-04-23 15:11:00 +0200

    Create-City-Collaborator-Role-Date-Model-Dto

---
+++ Repositories.Contracts/ICityRepository.cs
@@ -0,0 +1,21 @@
+﻿using DTOs.CityDTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface ICityRepository
+    {
+        Task AddAsync(CityAddDto cityAddDto);
+        CityGetDto GetByIdAsync(int id); 
+        List<CityGetDto> StartsWithAsync(string recherche);
+        Task UpdateAsync(CityUpdateDto cityUpdateDto);
+        Task DeleteAsync(int id);
+
+    }
+}
---
+++ Repositories.Contracts/IModelRepository.cs
@@ -0,0 +1,20 @@
+﻿using DTOs.ModelDTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts
+{
+    public interface IModelRepository
+    {
+        Task AddAsync(ModelAddDto modelAddDto);
+        ModelGetDto GetByIdAsync(int id);
+        List<ModelGetDto> GetAllAsync(); 
+        Task UpdateAsync(ModelUpdateDto modelUpdateDto);
+        Task DeleteAsync(int id);
+        List<ModelGetDto> GetByBrandAsync(Brand brand);
+    }
+}
