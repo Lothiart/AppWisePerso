94bfeca8f9fe5bf0229955cbf98e524ea44a12c0
Elsa <elsa.thievet@hotmail.fr>
2024-04-23 10:50:02 +0200

    Created IAddressRepository

---
+++ Repositories.Contracts/IAddressRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Contracts;
+public interface IAddressRepository
+{
+    Task AddAsync(AddressAddDto addressAddDto);
+    Task<> GetByIdAsync(int id);
+}
