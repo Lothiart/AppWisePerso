71639e5b71527f819e1c5a1873f1b24f4ff03731
Greg <Gregacket@yahoo.fr>
2024-04-25 11:03:06 +0200

    vehicledto add

--- Repositories.Contracts/obj/Debug/net8.0/Repositories.Contracts.AssemblyInfo.cs
+++ Repositories.Contracts/obj/Debug/net8.0/Repositories.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+182f8d284ffe74dcc3c06e32d3b50b4e48e2cb77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ace80c5d72820fa2e05d0de1bb176976c7212fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
