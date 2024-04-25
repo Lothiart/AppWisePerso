5bc370ccb62d8385a2a1b0a9c73b939da77aa488
Elsa <elsa.thievet@hotmail.fr>
2024-04-25 15:55:00 +0200

    Added methods to collab mapper and vehicle mapper

--- DTOs/Mappers/CollaboratorMapper.cs
+++ DTOs/Mappers/CollaboratorMapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DTOs.DTOs.CollaboratorDTOs;
+using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +9,16 @@ using System.Threading.Tasks;
 namespace DTOs.Mappers;
 public class CollaboratorMapper
 {
+    public CollaboratorGetDto CollaboratorToCollaboratorGetDto(Collaborator c)
+    {
+        CollaboratorGetDto collaboratorDto = new CollaboratorGetDto()
+        {
+            Id = c.Id,
+            AppUser = c.AppUser,
+            FirstName = c.FirstName,
+            LastName = c.LastName            
+        };
+
+        return collaboratorDto;
+    }
 }
--- DTOs/Mappers/VehicleMapper.cs
+++ DTOs/Mappers/VehicleMapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DTOs.DTOs.VehicleDTOs;
+using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,4 +9,17 @@ using System.Threading.Tasks;
 namespace DTOs.Mappers;
 public class VehicleMapper
 {
+    public VehicleGetDto VehicleToVehicleGetDto(Vehicle vehicle)
+    {
+        VehicleGetDto vehicleDto = new VehicleGetDto()
+        {
+            Id = vehicle.Id,
+            CategoryId = vehicle.CategoryId,
+            CO2EmissionKm = vehicle.CO2EmissionKm,
+            ModelId = vehicle.ModelId,
+            MotorId = vehicle.MotorId,
+            Registration = vehicle.Registration,
+            TotalSeats = vehicle.TotalSeats
+        };
+    }
 }
