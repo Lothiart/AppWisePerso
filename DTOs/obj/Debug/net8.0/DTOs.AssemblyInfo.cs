d607036ef81c3b902b4954ba6a73c6d12850c196
Greg <Gregacket@yahoo.fr>
2024-04-25 12:32:05 +0200

    mergefromDev

--- DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
+++ DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
@@ -13,11 +13,10 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b80f0008a2ea8b908e692635c6842e5db3c1f6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+decbc433cf6cf67bc97dc3cd75beacf6babc0ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
-// Généré par la classe MSBuild WriteCodeFragment.
 // Généré par la classe MSBuild WriteCodeFragment.
 
--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bfbd049ac05f15de7501065c109db907f923dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+decbc433cf6cf67bc97dc3cd75beacf6babc0ece")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
