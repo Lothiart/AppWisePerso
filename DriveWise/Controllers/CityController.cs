bb7e2ec2954001ff9d0e117c1a4feef8d796379e
Lothiart <cailllet.alex@gmail.com>
2024-04-25 13:51:39 +0200

    add controller

---
+++ DriveWise/Controllers/CityController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Repositories.Contracts;
+
+namespace DriveWise.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CityController(
+        ICityRepository cityRepository,
+        ILogger<CityController> logger) : ControllerBase
+    {
+
+
+
+
+
+    }
+}
