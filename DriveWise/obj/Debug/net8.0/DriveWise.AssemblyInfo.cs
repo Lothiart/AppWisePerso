decbc433cf6cf67bc97dc3cd75beacf6babc0ece
Greg <Gregacket@yahoo.fr>
2024-04-25 12:28:21 +0200

    Add Gregsrepo

--- DriveWise/obj/Debug/net8.0/DriveWise.AssemblyInfo.cs
+++ DriveWise/obj/Debug/net8.0/DriveWise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b80f0008a2ea8b908e692635c6842e5db3c1f6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0bfbd049ac05f15de7501065c109db907f923dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DriveWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
