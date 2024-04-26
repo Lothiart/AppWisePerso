6d17590d1d155817f1f91918c584c1c6440b56e9
Lothiart <cailllet.alex@gmail.com>
2024-04-26 16:29:24 +0200

    Fix

--- DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
+++ DTOs/obj/Debug/net8.0/DTOs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad992f24a431084c881ff62ebf392724f116482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67a4dbec9ea7449f66e4ba602d06cae248b8b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DTOs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Models/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad992f24a431084c881ff62ebf392724f116482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67a4dbec9ea7449f66e4ba602d06cae248b8b35")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
