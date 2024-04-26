facd5fc6a8d026982fbb387b5da006352c2a616d
Greg <Gregacket@yahoo.fr>
2024-04-26 16:12:39 +0200

    obj driveWise

--- DTOs/Mappers/CarpoolMapper.cs
+++ DTOs/Mappers/CarpoolMapper.cs
@@ -1,68 +1,68 @@
-﻿// using DTOs.DTOs.AddressDTOs;
-// using DTOs.DTOs.CollaboratorDTOs;
-// using DTOs.DTOs.CarpoolDTOs;
-// using Entities;
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Text;
-// using System.Threading.Tasks;
+﻿using DTOs.DTOs.AddressDTOs;
+using DTOs.DTOs.CollaboratorDTOs;
+using DTOs.DTOs.CarpoolDTOs;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-// namespace DTOs.Mappers;
-// public class CarpoolMapper(CollaboratorMapper collaboratorMapper,
-//                             VehicleMapper vehicleMapper,
-//                             AddressMapper addressMapper)
-// {
+namespace DTOs.Mappers;
+public class CarpoolMapper(CollaboratorMapper collaboratorMapper,
+                            VehicleMapper vehicleMapper,
+                            AddressMapper addressMapper)
+{
 
-//     #region EntityToDto
-//     public List<CarpoolGetDto> ListCarpoolToListCarpoolGetDto(List<Carpool> carpools)
-//     {
-//         List<CarpoolGetDto> carpoolDtos = new();
+    #region EntityToDto
+    public List<CarpoolGetDto> ListCarpoolToListCarpoolGetDto(List<Carpool> carpools)
+    {
+        List<CarpoolGetDto> carpoolDtos = new();
 
-//         foreach (Carpool carpool in carpools)
-//         {
-//             carpoolDtos.Add(CarpoolToCarpoolGetDto(carpool));
-//         }
+        foreach (Carpool carpool in carpools)
+        {
+            carpoolDtos.Add(CarpoolToCarpoolGetDto(carpool));
+        }
 
-//         return carpoolDtos;
-//     }
+        return carpoolDtos;
+    }
 
-//     public CarpoolGetDto CarpoolToCarpoolGetDto(Carpool carpool)
-//     {
-//         CarpoolGetDto carpoolDto = new CarpoolGetDto()
-//         {
-//             Id = carpool.Id,
-//             DriverId = carpool.DriverId,
-//             Driver = collaboratorMapper.CollaboratorToCollaboratorGetDto(carpool.Driver),
-//             DateId = carpool.DateId,
-//             VehicleGetDto = vehicleMapper.VehicleToVehicleGetDto(carpool.Vehicle),
-//             RemainingSeats = carpool.Vehicle.TotalSeats - carpool.Passengers.Count - 1, //-1 = driver seat
-//             StartAddressDto = addressMapper.AddressToAddressGetDto(carpool.StartAddress),
-//             EndAddressDto = addressMapper.AddressToAddressGetDto(carpool.EndAddress),
-//             PassengersDto = collaboratorMapper.ListCollaboratorToListCollaboratorGetDto(carpool.Passengers),
-//         };
+    public CarpoolGetDto CarpoolToCarpoolGetDto(Carpool carpool)
+    {
+        CarpoolGetDto carpoolDto = new CarpoolGetDto()
+        {
+            Id = carpool.Id,
+            DriverId = carpool.DriverId,
+            Driver = collaboratorMapper.CollaboratorToCollaboratorGetDto(carpool.Driver),
+            DateId = carpool.DateId,
+            VehicleGetDto = vehicleMapper.VehicleToVehicleGetDto(carpool.Vehicle),
+            RemainingSeats = carpool.Vehicle.TotalSeats - carpool.Passengers.Count - 1, //-1 = driver seat
+            StartAddressDto = addressMapper.AddressToAddressGetDto(carpool.StartAddress),
+            EndAddressDto = addressMapper.AddressToAddressGetDto(carpool.EndAddress),
+            PassengersDto = collaboratorMapper.ListCollaboratorToListCollaboratorGetDto(carpool.Passengers),
+        };
 
-//         return carpoolDto;
-//     }
+        return carpoolDto;
+    }
 
-//     #endregion
+    #endregion
 
 
-//     #region DtoToEntity
+    #region DtoToEntity
 
-//     public Carpool CarpoolAddDtoToCarpool (CarpoolAddDto carpoolDto)
-//     {
-//         Carpool c = new Carpool()
-//         {
-//             DateId = carpoolDto.DateId,
-//             DriverId = carpoolDto.DriverId,
-//             EndAddressId = carpoolDto.EndAddressId,
-//             StartAddressId = carpoolDto.StartAddressId,
-//             VehicleId = carpoolDto.VehicleId,
-//         };
+    public Carpool CarpoolAddDtoToCarpool(CarpoolAddDto carpoolDto)
+    {
+        Carpool c = new Carpool()
+        {
+            DateId = carpoolDto.DateId,
+            DriverId = carpoolDto.DriverId,
+            EndAddressId = carpoolDto.EndAddressId,
+            StartAddressId = carpoolDto.StartAddressId,
+            VehicleId = carpoolDto.VehicleId,
+        };
 
-//         return c;
-//     }
+        return c;
+    }
 
-//     #endregion
-// }
+    #endregion
+}
