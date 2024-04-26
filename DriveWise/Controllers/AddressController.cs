e33d707f5e2544ec35d1f6bd505a7c3d18cc1a26
Elsa <elsa.thievet@hotmail.fr>
2024-04-26 17:45:56 +0200

    added Address Controller

---
+++ DriveWise/Controllers/AddressController.cs
@@ -0,0 +1,16 @@
+﻿using DTOs.DTOs.AddressDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Contracts;
+
+namespace DriveWise.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AddressController(IAddressRepository addressRepository) : ControllerBase
+{
+    [HttpPost]   
+    public Task<ActionResult<AddressAddDto>> Add(AddressAddDto addressDto)
+    {
+        
+    }
+}
