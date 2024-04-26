cad992f24a431084c881ff62ebf392724f116482
Lothiart <cailllet.alex@gmail.com>
2024-04-26 15:36:51 +0200

    Add-Collaborator-Controller

--- DriveWise/Controllers/CollaboratorController.cs
+++ DriveWise/Controllers/CollaboratorController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DTOs.DTOs.CollaboratorDTOs;
+using Microsoft.AspNetCore.Mvc;
 using Repositories.Contracts;
 
 namespace DriveWise.Controllers
 {
     public class CollaboratorController(
-        ICityRepository cityRepository,
-        ILogger<CityController> logger) : ControllerBase
+        ICollaboratorRepository collaboratorRepository,
+        ILogger<CollaboratorController> logger) : ControllerBase
     {
-        
+
+        [HttpGet]
+        public async Task<IActionResult> GetByIdPerso(int id)
+        {
+            try
+            {
+                return Ok(await collaboratorRepository.GetByIdPersoAsync(id));
+            }
+            catch (Exception e)
+            {
+                return Problem(e!.InnerException!.Message);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+
+                return Ok(await collaboratorRepository.GetByIdAsync(id));
+            }
+            catch (Exception e)
+            {
+                return Problem(e!.InnerException!.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> GiveAdminRole(int id)
+        {
+            try
+            {
+                await collaboratorRepository.GiveAdminRoleAsync(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return Problem(e!.InnerException!.Message);
+            }
+        }
     }
 }
