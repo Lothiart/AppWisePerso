39d7a74082852aaf6d4b68c8bb5f172bfa5d51e8
Elsa <elsa.thievet@hotmail.fr>
2024-04-30 14:06:14 +0200

    added all carpool repository methods + minor fixes in carpool repo/irepo parameters and so on

---
+++ DTOs/DTOs/CarpoolDTOs/CarpoolSearchDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTOs.CarpoolDTOs;
+public class CarpoolSearchDto
+{
+    public DateTime DateId { get; set; }
+    public string StartCity { get; set; }
+    public string EndCity { get; set; }
+}
